358ea944309ec112313c5a2ceef6d2e6dec409b4
tolik7071 <tolik7071@gmail.com>
2016-11-26 12:48:17 +0200

    Adapter has been implemented.

---
+++ DesignPatterns/Adapter.cpp
@@ -0,0 +1,68 @@
+//
+//  Adapter.cpp
+//  DesignPatterns
+//
+//  Created by Anatoliy Goodz on 11/23/16.
+//  Copyright © 2016 Anatoliy Goodz. All rights reserved.
+//
+
+#include "Adapter.hpp"
+#include "common.h"
+
+using namespace Adapter;
+
+IShape::~IShape()
+{
+    LOG_FUNCTION();
+}
+
+void Line::Draw()
+{
+    LOG_FUNCTION();
+}
+
+void Polyline::Draw()
+{
+    LOG_FUNCTION();
+}
+
+void Text::Draw()
+{
+    LOG_FUNCTION();
+}
+
+void Drawer::DrawShapes() const
+{
+    std::for_each(mShapes.begin(), mShapes.end(), [](IShape* shape) { shape->Draw(); });
+}
+
+Drawer::~Drawer()
+{
+    std::for_each(mShapes.begin(), mShapes.end(), [](IShape* shape) { delete shape; });
+    LOG_FUNCTION();
+}
+
+void NewTextView::DrawText()
+{
+    LOG_FUNCTION();
+}
+
+NewTextView::~NewTextView()
+{
+    LOG_FUNCTION();
+}
+
+void NewTextViewAdapter::Draw()
+{
+    mTextView->DrawText();
+}
+
+NewTextViewAdapter::NewTextViewAdapter(std::unique_ptr<NewTextView> textView)
+{
+    mTextView = std::move(textView);
+}
+
+NewTextViewAdapter::~NewTextViewAdapter()
+{
+    LOG_FUNCTION();
+}
