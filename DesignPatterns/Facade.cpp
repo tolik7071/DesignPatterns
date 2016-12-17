e941724f0786ab005d0bd7dbf0478dc2cd88a046
tolik7071 <tolik7071@gmail.com>
2016-12-17 10:31:43 +0200

    stdafx.h has been added to some cpp files.

--- DesignPatterns/Facade.cpp
+++ DesignPatterns/Facade.cpp
@@ -6,6 +6,9 @@
 //  Copyright © 2016 Anatoliy Goodz. All rights reserved.
 //
 
+#if defined(_MSC_VER)
+#include "stdafx.h"
+#endif // _MSC_VER
 #include "Facade.hpp"
 #include "common.h"
 #include <algorithm>
